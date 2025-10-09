0bedab50ec3f1821f4671772494239ea22516a9d
服部半藏 <oncwnuNoM3ZiyXHw5Ei9BEnsOQI0@git.weixin.qq.com>
2025-10-10 00:04:02 +0800

    更新flow工作流没有正确使用id问题

--- frontend/platform/src/pages/AdminNewPage/ChatFlowAuthSubRoute.tsx
+++ frontend/platform/src/pages/AdminNewPage/ChatFlowAuthSubRoute.tsx
@@ -3,14 +3,14 @@ import NewChatPro from '../NewChatPage/index';
 import { AppNumType } from '@/types/app';
 
 interface ChatFlowAuthSubRouteProps {
-  id?: string;
+  flowId?: string;
 }
 
-export default function ChatFlowAuthSubRoute({ id }: ChatFlowAuthSubRouteProps) {
+export default function ChatFlowAuthSubRoute({ flowId }: ChatFlowAuthSubRouteProps) {
   // 如果没有提供ID，使用默认的聊天ID
-  const chatId = id || "1b87aea2a46b40febe3e0d8bc10364bf";
+  const chatId = flowId || "1b87aea2a46b40febe3e0d8bc10364bf";
   
-  // console.log('ChatFlowAuthSubRoute - 接收到的ID:', id, '使用的chatId:', chatId);
+  console.log('ChatFlowAuthSubRoute - 接收到的flowId:', flowId, '使用的chatId:', chatId);
   
   return React.createElement("div", { className: "h-full w-full" },
    
